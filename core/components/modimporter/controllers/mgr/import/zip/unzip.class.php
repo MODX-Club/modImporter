15cfb3493bca67dbbae813c94fcb94d65d7420d8
root <root@dev2.shopmodx.ru>
2016-01-12 19:42:27 +0300

    empty controllers

---
+++ core/components/modimporter/controllers/mgr/import/zip/unzip.class.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(dirname(__FILE__)). '/index.class.php';
+
+class ModimporterControllersMgrImportZipUnzipManagerController extends ModimporterControllersMgrImportIndexManagerController{}
